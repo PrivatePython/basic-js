acefac8ce967e5207e7ee8bca709f00125b0bea7
privatePython <gitarist.pasha@gmail.com>
2025-01-05 23:13:00 +0300

    feat: add solutions for common-character-count and count-cats tasks

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  return matrix.flat().reduce((counter, value) => {
+    if (value === '^^') {
+      return counter + 1;
+    }
+    return counter;
+  }, 0);
 }
 
 module.exports = {
